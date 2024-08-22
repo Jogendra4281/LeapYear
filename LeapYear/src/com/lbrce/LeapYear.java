ecec6a7bd3db44d4b8bb76e2e284f688cb4c9c4d
SravaniMunganti <130158684+SravaniMunganti@users.noreply.github.com>
2024-08-22 10:40:59 +0530

    Update LeapYear.java

--- LeapYear/src/com/lbrce/LeapYear.java
+++ LeapYear/src/com/lbrce/LeapYear.java
@@ -6,5 +6,11 @@ public class LeapYear {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD!!!......");
 	}
+public class LeapYear2{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JOS");
+	}
 
 }
